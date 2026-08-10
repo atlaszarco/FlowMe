e8637283c6b67c6bd4b0521f76cb231199386c22
atlas <63524806+atlaszarco@users.noreply.github.com>
2026-08-10 15:38:02 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'flowme-v1.6.93';
-const urlsToCache = ['./', './index.html', './manifest.json'];
+const urlsToCache = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png',];
 
 // Instala e guarda a versão inicial
 self.addEventListener('install', (event) => {

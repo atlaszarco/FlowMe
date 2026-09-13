57a681c10b35fe0a04a70946d6e397533aaf7596
atlas <63524806+atlaszarco@users.noreply.github.com>
2026-09-13 18:30:00 -0300

    Update CACHE_NAME to version 4.2.5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'flowme-vbeta-4.0.9';
+const CACHE_NAME = 'flowme-vbeta-4.2.5';
 const urlsToCache = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png',];
 
 // Instala e guarda a versão inicial
